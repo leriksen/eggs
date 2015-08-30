e2689914285bc13b7380e3840a1e22a15fd270dc
leif.eriksen <leif.eriksen@iag.com.au>
2015-08-30 18:49:02 +1000

    get menu bar working

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :first_name, presence:   true
+  validates :last_name , presence:   true
+  validates :password  , presence:   true
+  validates :email     , presence:   true,
+                         uniqueness: true,
+                         email:      true
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'pages/:id' => 'pages#show'
 
   root 'pages#index'
