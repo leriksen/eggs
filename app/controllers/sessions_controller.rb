01adcc978e595b9cb841a7f3633e98bd410d1d05
leif.eriksen <leif.eriksen@iag.com.au>
2015-08-30 22:04:44 +1000

    fleshing out login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user and user.authenticate(params[:session][:password])
+      session[:user] = user.id
+      flash[:success] = "Successfully logged in"
+      redirect_to root_url
+    else
+      flash[:error] = "Invalid email/password combination"
+      render :new
+    end
   end
 
   def destroy
+    session[:user] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_url
   end
 end
