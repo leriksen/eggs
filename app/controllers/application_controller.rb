5dc8f5cf0ed40455a56d3dadaf84bb22954605d4
leif.eriksen <leif.eriksen@iag.com.au>
2015-09-01 07:20:39 +1000

    use more standard sessions support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-  def current_user
-    @current_user = User.where(id: session[:user]).first
-  end
-
-  helper_method :current_user
+  include SessionsHelper
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user])
+  end
 end
