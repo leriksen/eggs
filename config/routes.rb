27c3fa2770be930c56503a6a09cbfbb884f61868
leif.eriksen <leif.eriksen@iag.com.au>
2015-09-07 09:58:42 +1000

    tray type support, for adding to trays

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tray_types
   resources :reports
   resources :trays
   resources :sheds
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906130000) do
+ActiveRecord::Schema.define(version: 20150906235023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "sheds", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tray_types", force: :cascade do |t|
+    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
