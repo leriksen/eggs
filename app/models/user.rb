3a5c2503c529cd356d40b9507ef0c54fe4d2c056
leif.eriksen <leif.eriksen@iag.com.au>
2015-09-06 11:40:58 +1000

    make uniqueness validation case-insensitive as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true, length: {within: (2..20)}
   validates :last_name, presence: true, length: {within: (2..20)}
   validates :password, presence: true, length: {within: (6..255)}
-  validates :email, presence: true, length: {maximum: 255}, uniqueness: true, email: true
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}, email: true
 
   before_save { self.email = email.downcase }
-  
+
   def to_s
     "#{first_name} #{last_name}"
   end
