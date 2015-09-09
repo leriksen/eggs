6b42d66f4333b97f4bfd96b95b4eb1ec0cfb34b8
leif.eriksen <leif.eriksen@iag.com.au>
2015-09-09 13:10:55 +1000

    improve layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
 
   has_many :trays
   
-  validates :first_name, presence: true, length: {within: (2..20)}
-  validates :last_name, presence: true, length: {within: (2..20)}
-  validates :password, presence: true, length: {within: (6..255)}
-  validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}, email: true
+  validates :first_name, presence: true,  length:     {within: (2..20)}
+  validates :last_name,  presence: true,  length:     {within: (2..20)}
+  validates :password,   presence: true,  length:     {within: (6..255)}
+  validates :email,      presence: true,  length:     {maximum: 255},
+                                          uniqueness: {case_sensitive: false},
+                                          email: true
 
   before_save { self.email = email.downcase }
 
