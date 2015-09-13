c8e452014f168f0b6d2de221f796009f57c9d586
leif.eriksen <leif.eriksen@iag.com.au>
2015-09-13 23:35:36 +1000

    cleanout unrequired views/flocks/new.html.haml only show the 'active' checkbox if on edit page, not on signup page remove back link on signup page, so we dont end up on users index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController  
   def create
     user = User.find_by_email(params[:session][:email])
-    if user and user.authenticate(params[:session][:password])
+    if user and user.active == false
+      flash.now[:error] = "User is marked inactive, and cannot log in"
+      render :new
+    elsif user and user.authenticate(params[:session][:password])
       log_in user
       flash[:success] = "Successfully logged in"
       params[:session][:remember_me] == '1' ?
