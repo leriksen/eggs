860776c4a930dce7cb79f848d3453e960b17ea6d
leif.eriksen <leif.eriksen@iag.com.au>
2015-09-16 10:40:18 +1000

    begin support for counting total eggs, form needs refining

--- app/models/run.rb
+++ app/models/run.rb
@@ -26,4 +26,41 @@ class Run < ActiveRecord::Base
                                     only_integer: true,
                                     greater_than_or_equal_to: 0
                                   }
+
+  EGGS_PER_TRAY = 36
+
+  class << self
+    def build_virtual_attrs
+      %w(delivered standard seconds jumbo).each do |type|
+        %w(trays singles).each do |amount|
+          define_method "#{type}_#{amount}" do
+            value = send("#{type}")
+            case amount
+            when "trays"
+              value.present? ? (value / EGGS_PER_TRAY).to_i : 0
+            when "singles"
+              value.present? ? value % EGGS_PER_TRAY : 0
+            end
+          end
+
+          define_method "#{type}_#{amount}=" do |new_value|
+            value = send("#{type}")
+            value ||= 0
+            new_value = new_value.to_i
+            case amount
+            when "trays"
+              value += new_value * EGGS_PER_TRAY
+            when "singles"
+              value += new_value
+            end
+            send("#{type}=", value)
+            save
+          end
+        end
+      end
+    end
+  end
+
+  build_virtual_attrs
+
 end
